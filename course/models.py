0b5c6673dd7edaeeb56e45fce41719f3eb93ae31
Hanifah Abatcha <habatcha@berkeley.edu>
2023-03-14 19:41:27 -0700

    user2

--- course/models.py
+++ course/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from hikmah.user.models import Teacher
 # 
 # Create your models here.
+class Course(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    department = models.CharField(max_length=200)
+    semester = models.CharField(max_length=200)
+
