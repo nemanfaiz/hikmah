631eafc8fb5969523247479ea842d6349f57a950
Hanifah Abatcha <habatcha@berkeley.edu>
2023-03-20 12:51:59 -0700

    finished coursed.models

--- course/models.py
+++ course/models.py
@@ -8,6 +8,23 @@ class Course(models.Model):
     name = models.CharField(max_length=200)
     department = models.CharField(max_length=200)
     semester = models.CharField(max_length=200)
+
 class StudentGrade(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(Course, on_delete= models.CASCADE)
+    grade = models.CharField(max_length = 200)
+
+class StudentCourse(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    gradeID = models.ForeignKey(StudentGrade, on_delete=models.CASCADE)
+
+class Assignment(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class StudentAssignment(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    assignment_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
     
